6d459b0560d2fe547921e2510ecda6b4e3602b0d
Carlos Alcala <alcala21@gmail.com>
2021-09-30 15:15:20 -0500

    Flashcards. Stage 3/7.

--- flashcards.py
+++ flashcards.py
@@ -1,22 +1,33 @@
 # Write your code here
+from random import choice
+
 class FlashCards:
     def __init__(self):
-        pass
+        self.cards = []
 
     def start(self):
-        term = input()
-        definition = input()
-        answer = input()
-        if answer == definition:
-            print('Your answer is right!')
-        else:
-            print('Your answer is wrong!')
+        self.build_deck()
+        self.test_user()
 
-    def print(self):
-        print("Card:")
-        print('House')
-        print('Definition:')
-        print('Party')
+    def build_deck(self):
+        print("Input the number of cards:")
+        n = int(input())
+
+        for i in range(1, n + 1):
+            print(f"The term for card #{i}:")
+            term = input()
+            print(f"The definition for card #{i}:")
+            definition = input()
+            self.cards.append({'term': term, 'definition': definition})
+
+    def test_user(self):
+        for card in self.cards:
+            print(f'Print the definition of "{card["term"]}":')
+            answer = input()
+            if answer == card["definition"]:
+                print('Correct!')
+            else:
+                print(f'Wrong. The right answer is "{card["definition"]}"')
 
 
 FlashCards().start()
